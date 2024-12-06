336564ea878ae713d2ed4ae7fab6fdd70194b0c1
The842 <123998630+The842@users.noreply.github.com>
2024-12-06 02:08:51 +0000

    The MedicalRecordController is being created. I added the viewMedicalRecord method.

--- ClinicManagementService/src/main/java/org/example/controller/MedicalRecordController.java
+++ ClinicManagementService/src/main/java/org/example/controller/MedicalRecordController.java
@@ -1,4 +1,28 @@
 package org.example.controller;
 
+import org.example.model.MedicalRecordModel;
+import org.example.util.MedicalRecordDML;
+
 public class MedicalRecordController {
+
+    private MedicalRecordDML medicalRecordDML;
+
+    public MedicalRecordController() {
+        this.medicalRecordDML = new MedicalRecordDML();
+    }
+
+    /**
+     * View the medical record of a patient by their patient ID.
+     * @param patientId the ID of the patient
+     */
+    public void viewMedicalRecord(int patientId) {
+        MedicalRecordModel medicalRecord = MedicalRecordDML.getMedicalRecordById(patientId);
+
+        if (medicalRecord != null) {
+            System.out.println("Medical Record for Patient ID: " + patientId);
+            System.out.println(medicalRecord);
+        } else {
+            System.out.println("Medical Record not found for patient ID: " + patientId);
+        }
+    }
 }
